4591862bac68cb8a222947feddd7c43d342b1d2a
Ross Dobson <56548895+ross-dobson@users.noreply.github.com>
2023-06-21 16:35:59 +0100

    get_ids_fit.py tidied, actually works

--- get_ids_fit.py
+++ get_ids_fit.py
@@ -1,13 +1,14 @@
-"""Ross Dobson 2023-06-15, rdobson@ing.iac.es"""
+"""Ross Dobson 2023-06-20, rdobson@ing.iac.es"""
 
 import sys
 from datetime import date, timedelta
 from pathlib import Path
 import shutil
+from astropy.io import fits
 
-obsdata_inta = Path("/obsdata/inta")
+path_obsdata_inta = Path("/obsdata/inta")
 
-# 1: Check command-line arguments
+# Step 1: Check command-line arguments
 if len(sys.argv) != 4:
     print("Usage: get_ids_fit.py detector obstype YYYYMMDD")
     sys.exit(1)
@@ -18,14 +19,14 @@ arg_detector = sys.argv[1]
 
 if not (sys.argv[2] == "ARC" or sys.argv[2] == "BIAS" or sys.argv[2] == "DARK"
     or sys.argv[2] == "FLASH" or sys.argv[2] == "FLAT" or sys.argv[2] == "SKY"
-    or sys.argv[2] == "TARGET"):
+    or sys.argv[2] == "TARGET" or sys.argv[2] == "ALL"):
     raise ValueError("Obstype can be ARC, BIAS, DARK, FLASH, FLAT, SKY, TARGET or ALL.")
 arg_obstype = sys.argv[2]
 
 if not (sys.argv[3] == "ALL"):
     bool_all_dates = False
+    arg_date = sys.argv[3]
     try:
-        arg_date = sys.argv[3]
         yyyymmdd = date(year=int(arg_date[0:4]), month=int(arg_date[4:6]), day=int(arg_date[6:8]))
     except:
         raise ValueError("Date can be in format YYYYMMDD, or ALL")
@@ -33,43 +34,74 @@ else:
     arg_date = sys.argv[3]
     bool_all_dates = True
 
+print(f"Requested image types: \nDetector: {arg_detector} \nObstype: {arg_obstype} \nDate: {arg_date}")
 
-print(sys.argv)
-print(arg_detector, arg_obstype, arg_date)
-sys.exit(0)
+# Step 2: get list of valid fits
 
-# step 2: make a directory for this day. copy all the fits files into it
+def get_list_of_fits(day, detector, obstype):
+    """docstring lolololol"""
+    list_fits_final = []
+    path_int_yyyymmdd = path_obsdata_inta / day
+    if not Path.exists(path_int_yyyymmdd):
+        raise Exception(f"Dir {path_int_yyyymmdd} not found")
+    print(f"\nLooking in {path_int_yyyymmdd}")
 
-for d in date_list:
-    d_str = d.strftime("%Y%m%d")
-    print(f"\n{d_str}")
+    # get list of all .fit file in the folders
+    list_of_all_fits = list(path_int_yyyymmdd.glob("*.fit"))
 
-    # find corresponding day's folder on INT system
-    todays_INT_path = obsdata_inta / d_str
-    print("DEBUG INT path:", todays_INT_path)
+    # get list of the .fit that match the requested detector and obstype
+    for f in list_of_all_fits:
+        hdul = fits.open(f)
+        hdr0 = hdul[0].header
+        if not hdr0['INSTRUME'] == "IDS":
+            continue
+        hdr0_obstype = hdr0['OBSTYPE']
+        hdr0_detector = hdr0['DETECTOR']
 
-    # check that INT folder exists
-    if not Path.exists(todays_INT_path):
-        raise Exception(f"INT path {todays_INT_path} not found")
+        if arg_obstype == "ALL" or arg_obstype == hdr0_obstype:
+            if arg_detector == "BOTH":
+                if hdr0_detector == "REDPLUS2" or hdr0_detector == "EEV10":
+                    list_fits_final.append(f)
+            elif arg_detector == hdr0_detector:
+                list_fits_final.append(f)
 
-    # make a list of fit files in the INT folder
-    todays_INT_fits = list(todays_INT_path.glob("*.fit"))
+    if len(list_fits_final) == 0:
+        print(f"No fits files matching detector {detector}, obstype {obstype} found for {day}")
+        return
+    else:
+        print(f"Found {len(list_fits_final)} fits files matching detector {detector}, obstype {obstype} for day {day}")
 
-    # check INT folder is not empty. If empty, move on to tomorrow
-    if len(todays_INT_fits) == 0:
-        print(f"No .fit files in {todays_INT_fits}, skipping this day")
-        continue
-
-    # make a working directory for this date's fits file
-    todays_dir = Path.cwd() / d_str
+    # make a directory to store this date's fits files
+    path_cwd_yyyymmdd = Path.cwd() / day
     try:
-        todays_dir.mkdir()
-        print(f"Creating dir {todays_dir}")
+        path_cwd_yyyymmdd.mkdir()
+        print(f"Creating dir {path_cwd_yyyymmdd}")
     except FileExistsError:
-        print(f"{todays_dir} already exists")
+        print(f"{path_cwd_yyyymmdd} already exists")
 
-    # copy the fits from INT to local home
-    for INT_fit_file in todays_INT_fits:
-        local_fit_file = todays_dir/INT_fit_file.name
-        print("Copying", INT_fit_file, "to", local_fit_file)
-        shutil.copyfile(INT_fit_file, local_fit_file)
+    # copy the fits files from /obsdata/inta/yyyymmdd to the ./yyyymmdd directory
+    for f in list_fits_final:
+        intf = path_int_yyyymmdd / f.name
+        cwdf = path_cwd_yyyymmdd / f.name
+        if not cwdf.exists():
+            print(f"Copying {intf} to {cwdf}")
+            shutil.copyfile(intf, cwdf)
+        else:
+            print(f"File {cwdf} already exists")
+    return
+
+if not bool_all_dates:
+    get_list_of_fits(arg_date, arg_detector, arg_obstype)
+    sys.exit(0)
+elif bool_all_dates:
+    # List all the directories in /obsdata/inta. From the paths, get the dir
+    # name - this is in format yyyymmdd, so we can pass this into the function
+    # as the day argument. We don't just glob for **/*.fit as it would be much
+    # hard to copy in to (and if needed, mkdir) the local yyyymmdd directory.
+    list_of_int_dirs = [x for x in path_obsdata_inta.iterdir() if x.is_dir()]
+    list_days = [d.parts[-1] for d in list_of_int_dirs]
+    for day in list_days:
+        get_list_of_fits(day, arg_detector, arg_obstype)
+    sys.exit(0)
+
+sys.exit(1)
